a5226875d34435c7109cd16badfe6b997f5647bf
mayklweb <muhammadevvv@gmail.com>
2025-06-03 23:29:42 +0500

    design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -250,6 +250,85 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <section className="w-full mt-10">
+        <div className="container">
+          <h1 className="text-4xl lg:text-6xl font-bold">TREND</h1>
+          <div className="mt-5 lg:mt-10 w-full flex flex-col lg:flex-row gap-5">
+            <div className="w-full lg:w-[25%] flex lg:flex-col gap-5">
+              <div className="w-full h-full lg:mt-10">
+                <div className="w-full h-[180px] lg:h-[400px] overflow-hidden rounded-[100%]">
+                  <img
+                    className="w-full h-full object-cover"
+                    src="/image.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="mt-2 flex flex-col items-center justify-between">
+                  <h3 className="text-[14px] lg:text-xl">CHANEL</h3>
+                  <p className="text-[14px] lg:text-xl">1 000 000 USZ</p>
+                </div>
+              </div>
+              <div className="w-full h-full lg:mt-10">
+                <div className="w-full h-[180px] lg:h-[400px] overflow-hidden rounded-[100%]">
+                  <img
+                    className="w-full h-full object-cover"
+                    src="/image.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="mt-2 flex flex-col items-center justify-between">
+                  <h3 className="text-[14px] lg:text-xl">CHANEL</h3>
+                  <p className="text-[14px] lg:text-xl">1 000 000 USZ</p>
+                </div>
+              </div>
+            </div>
+            <div className="w-full lg:w-[50%] h-full">
+              <div className="w-full h-full lg:mt-20">
+                <div className="w-full h-[380px] lg:h-[780px] overflow-hidden rounded-[100%]">
+                  <img
+                    className="w-full h-full object-cover"
+                    src="/image.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="mt-2 flex flex-col items-center justify-between">
+                  <h3 className="text-xl">BLEU DE CHANEL</h3>
+                  <p className="text-xl">1 000 000 USZ</p>
+                </div>
+              </div>
+            </div>
+            <div className="w-full lg:w-[25%] flex lg:flex-col gap-5">
+              <div className="w-full h-full lg:mt-10">
+                <div className="w-full h-[180px] lg:h-[400px] overflow-hidden rounded-[100%]">
+                  <img
+                    className="w-full h-full object-cover"
+                    src="/image.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="mt-2 flex flex-col items-center justify-between">
+                  <h3 className="text-[14px] lg:text-xl">CHANEL</h3>
+                  <p className="text-[14px] lg:text-xl">1 000 000 USZ</p>
+                </div>
+              </div>
+              <div className="w-full h-full lg:mt-10">
+                <div className="w-full h-[180px] lg:h-[400px] overflow-hidden rounded-[100%]">
+                  <img
+                    className="w-full h-full object-cover"
+                    src="/image.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="mt-2 flex flex-col items-center justify-between">
+                  <h3 className="text-[14px] lg:text-xl">CHANEL</h3>
+                  <p className="text-[14px] lg:text-xl">1 000 000 USZ</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
