136cb0af88c32176cf9c996092c15635128c3e2a
mayklweb <muhammadevvv@gmail.com>
2025-06-09 14:54:24 +0500

    products

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,5 +1,33 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Product() {
-  return <div>Product</div>;
+  return (
+    <section className="w-full">
+      <div className="container">
+        <div className="flex flex-col gap-2">
+          <div className="w-full h-[360px] rounded-t-[100%] overflow-hidden">
+            <Image
+              width={280}
+              height={360}
+              className="w-full h-full "
+              src="/image.jpg"
+              alt="Product Image"
+            />
+          </div>
+          <div>
+            <h1 className="text-xl">Mercedes Benz Woman In Red</h1>
+            <p className="">650 000 USZ</p>
+            <button className="w-full h-10 bg-[#090909]">SAVATGA</button>
+          </div>
+          <div>
+            <h3>opisaniya</h3>
+          </div>
+          <div>
+            <h1>recommended</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
