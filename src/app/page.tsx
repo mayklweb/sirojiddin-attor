1a981abb7fbce34a544b6b802fdaa1e12b12f929
mayklweb <muhammadevvv@gmail.com>
2025-06-28 12:09:47 +0500

    m

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
       </section> */}
 
       <section>
-        <div className="w-full h-[100vh] overflow-hidden flex items-center justify-center">
+        <div className="w-full h-[100dvh] overflow-hidden flex items-center justify-center">
           <div className="w-full h-full flex items-center lg:items-end justify-center relative">
             <div className="w-10/12 lg:w-1/3 h-[60%] lg:h-[80%] rounded-t-full overflow-hidden relative flex z-[3]">
               <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-[1]">
