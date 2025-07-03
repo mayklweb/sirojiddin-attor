482cab709428d8c5038254cf54fe1976a700f823
mayklweb <muhammadevvv@gmail.com>
2025-07-03 17:30:48 +0500

    m

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,6 @@ export default function Home() {
     return () => ctx.revert();
   }, []);
 
-
   const logos = [
     {
       logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsvCtsMoSgohb1s2BzErTa5aHK2EsKyDZvog&s",
@@ -122,10 +121,43 @@ export default function Home() {
             </div>
             <div className="w-full h-full absolute top-0 left-0 flex flex-col lg:flex-row items-center justify-evenly z-[3] mix-blend-difference">
               <div className="h-full flex items-center justify-evenly flex-col">
-                <h1 className="font-cormorant-garamond text-4xl text-center lg:text-6xl text-white italic tracking-tight">
+                <div className="w-full h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                  <svg
+                    width="600"
+                    height="600"
+                    viewBox="0 0 600 600"
+                    className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center"
+                  >
+                
+                    <defs>
+                      <path
+                        id="halfArc"
+                        d="M 130,0 A 110,190 0 0,1 480,210"
+                        fill="none"
+                      />
+                    </defs>
+                    <text
+                      className="w-full h-full"
+                      fill="#fff"
+                      fontSize="44"
+                      fontFamily="'Cormorant Garamond', serif"
+                      textAnchor="middle"
+                    >
+                      <textPath
+                        href="#halfArc"
+                        startOffset="50%"
+                        textLength="580"
+                      >
+                        Go‘zallik ifori bilan boshlanadi.
+                      </textPath>
+                    </text>
+                  </svg>
+                </div>
+                {/* <h1 className="font-cormorant-garamond text-4xl text-center lg:text-6xl rounded-full inline-block text-white italic tracking-tight">
+                  Go‘zallik ifori bilan boshlanadi.
                   Har bir iforda o‘ziga xoslik <br /> Biz bilan go‘zallikni his
                   eting!
-                </h1>
+                </h1> */}
                 <p></p>
               </div>
               <div className="h-full flex items-center justify-between flex-col">
@@ -161,7 +193,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="w-full mt-10 hidden">
+      {/* <section className="w-full mt-10 hidden">
         <div className="container">
           <h1 className="text-6xl font-bold">WOMEN</h1>
           <div className="mt-10 w-full flex gap-5">
@@ -268,7 +300,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
 
       <section className="w-full mt-10">
         <div className="container">
@@ -332,79 +364,37 @@ export default function Home() {
 
       <section className="w-full mt-10">
         <div className="container">
-          <h1 className="text-4xl lg:text-6xl font-bold">TREND</h1>
+          {/* <h1 className="text-4xl lg:text-6xl font-bold">TREND</h1> */}
           <div className="mt-5 lg:mt-10 w-full flex flex-col lg:flex-row gap-5">
-            <div className="w-full lg:w-[25%] flex lg:flex-col gap-5">
-              <div className="w-full h-full lg:mt-10">
-                <div className="w-full h-[180px] lg:h-[400px] overflow-hidden rounded-t-full">
-                  <img
-                    className="w-full h-full object-cover"
-                    src="/image.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="mt-2 flex flex-col items-center justify-between">
-                  <h3 className="text-[14px] lg:text-xl">CHANEL</h3>
-                  <p className="text-[14px] lg:text-xl">1 000 000 USZ</p>
-                </div>
-              </div>
-              <div className="w-full h-full lg:mt-10">
-                <div className="w-full h-[180px] lg:h-[400px] overflow-hidden rounded-t-full">
-                  <img
-                    className="w-full h-full object-cover"
-                    src="/image.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="mt-2 flex flex-col items-center justify-between">
-                  <h3 className="text-[14px] lg:text-xl">CHANEL</h3>
-                  <p className="text-[14px] lg:text-xl">1 000 000 USZ</p>
-                </div>
-              </div>
+            <div className="w-full lg:w-[100%] flex lg:flex-col gap-5 items-center">
+              <h1 className="w-[1024px] text-4xl font-cormorant-garamond text-center italic">
+                Parfyum — bu inson tanasiga, kiyimiga yoki atrof-muhitga yoqimli
+                hid berish uchun ishlatiladigan suyuqlik bo‘lib, u turli xildagi
+                efir moylari, aromatik birikmalar, erituvchilar (odatda spirt),
+                va suvdan iborat bo‘ladi. Uning asosiy vazifasi – hid orqali
+                kayfiyatni ko‘tarish, xotiralarni uyg‘otish yoki shaxsiy uslubni
+                ifodalashdir.
+              </h1>
             </div>
-            <div className="w-full lg:w-[50%] h-full">
-              <div className="w-full h-full lg:mt-20">
-                <div className="w-full h-[380px] lg:h-[780px] overflow-hidden rounded-t-full">
-                  <img
-                    className="w-full h-full object-cover"
-                    src="/image.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="mt-2 flex flex-col items-center justify-between">
-                  <h3 className="text-xl">BLEU DE CHANEL</h3>
-                  <p className="text-xl">1 000 000 USZ</p>
-                </div>
-              </div>
+
+            {/* <div className="w-full lg:w-[30%] h-[380px] lg:h-[620px] overflow-hidden rounded-t-full">
+              <img
+                className="w-full h-full object-cover"
+                src="/image.jpg"
+                alt=""
+              />
             </div>
-            <div className="w-full lg:w-[25%] flex lg:flex-col gap-5">
-              <div className="w-full h-full lg:mt-10">
-                <div className="w-full h-[180px] lg:h-[400px] overflow-hidden rounded-t-full">
-                  <img
-                    className="w-full h-full object-cover"
-                    src="/image.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="mt-2 flex flex-col items-center justify-between">
-                  <h3 className="text-[14px] lg:text-xl">CHANEL</h3>
-                  <p className="text-[14px] lg:text-xl">1 000 000 USZ</p>
-                </div>
-              </div>
-              <div className="w-full h-full lg:mt-10">
-                <div className="w-full h-[180px] lg:h-[400px] overflow-hidden rounded-t-full">
-                  <img
-                    className="w-full h-full object-cover"
-                    src="/image.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="mt-2 flex flex-col items-center justify-between">
-                  <h3 className="text-[14px] lg:text-xl">CHANEL</h3>
-                  <p className="text-[14px] lg:text-xl">1 000 000 USZ</p>
-                </div>
-              </div>
-            </div>
+
+            <div className="w-full lg:w-[35%] flex lg:flex-col gap-5">
+              <h1 className="text-5xl font-cormorant-garamond text-justify italic">
+                Parfyumdan foydalanish tarixi qadimgi Misr, Hindiston va
+                Mesopotamiya davrlariga borib taqaladi. Dastlab u diniy
+                marosimlarda, keyinchalik esa qirollik zodagonlari orasida
+                ishlatilgan. Yevropada parfyum ayniqsa Frantsiyada mashhur
+                bo‘lgan, va hozir ham Frantsiya dunyodagi eng yirik parfyumeriya
+                markazlaridan biri hisoblanadi.
+              </h1>
+            </div> */}
           </div>
         </div>
       </section>
@@ -413,9 +403,9 @@ export default function Home() {
         <div className="container">
           <h1 className="text-4xl lg:text-6xl font-bold">TREND</h1>
           <div className="mt-5 lg:mt-10 w-full flex flex-col lg:flex-row gap-5">
-            <div className="w-full lg:w-[25%] flex lg:flex-col gap-5">
+            <div className="w-full lg:w-[30%] flex lg:flex-col gap-5">
               <div className="w-full h-full lg:mt-10">
-                <div className="w-full h-[180px] lg:h-[400px] overflow-hidden rounded-t-full">
+                <div className="w-full h-[180px] lg:h-[500px] overflow-hidden rounded-t-full">
                   <img
                     className="w-full h-full object-cover"
                     src="/image.jpg"
@@ -428,7 +418,7 @@ export default function Home() {
                 </div>
               </div>
               <div className="w-full h-full lg:mt-10">
-                <div className="w-full h-[180px] lg:h-[400px] overflow-hidden rounded-t-full">
+                <div className="w-full h-[180px] lg:h-[500px] overflow-hidden rounded-t-full">
                   <img
                     className="w-full h-full object-cover"
                     src="/image.jpg"
@@ -441,9 +431,9 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="w-full lg:w-[50%] h-full">
-              <div className="w-full h-full lg:mt-20">
-                <div className="w-full h-[380px] lg:h-[780px] overflow-hidden rounded-t-full">
+            <div className="w-full lg:w-[40%] h-full flex items-center justify-center">
+              <div className="w-full h-full lg:mt-20 flex items-center justify-center flex-col">
+                <div className="w-full h-[380px] lg:h-[720px] overflow-hidden rounded-t-full">
                   <img
                     className="w-full h-full object-cover"
                     src="/image.jpg"
@@ -456,9 +446,9 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="w-full lg:w-[25%] flex lg:flex-col gap-5">
+            <div className="w-full lg:w-[30%] flex lg:flex-col gap-5">
               <div className="w-full h-full lg:mt-10">
-                <div className="w-full h-[180px] lg:h-[400px] overflow-hidden rounded-t-full">
+                <div className="w-full h-[180px] lg:h-[500px] overflow-hidden rounded-t-full">
                   <img
                     className="w-full h-full object-cover"
                     src="/image.jpg"
@@ -471,7 +461,7 @@ export default function Home() {
                 </div>
               </div>
               <div className="w-full h-full lg:mt-10">
-                <div className="w-full h-[180px] lg:h-[400px] overflow-hidden rounded-t-full">
+                <div className="w-full h-[180px] lg:h-[500px] overflow-hidden rounded-t-full">
                   <img
                     className="w-full h-full object-cover"
                     src="/image.jpg"
