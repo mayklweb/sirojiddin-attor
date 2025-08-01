b4664b7f2938174579cc1ec1d1edfaefca887d4d
mayklweb <muhammadevvv@gmail.com>
2025-08-01 11:32:07 +0500

    m

--- src/app/home/info/index.tsx
+++ src/app/home/info/index.tsx
@@ -3,7 +3,6 @@
 import gsap from "gsap";
 import { useEffect, useRef } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Image from "next/image";
 gsap.registerPlugin(ScrollTrigger);
 
 function Info() {
