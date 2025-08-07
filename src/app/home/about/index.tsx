1ecf233c03d0949962fdc2835bf8f6de2da38015
mayklweb <muhammadevvv@gmail.com>
2025-08-07 15:53:33 +0500

    m

--- src/app/home/about/index.tsx
+++ src/app/home/about/index.tsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <section>
       <div className="container">
-        <div className='pt-20 mt-40'>
+        <div className='pt-20 mt-20 lg:mt-40'>
           <h1 className="text-[#DAC289] text-center text-4xl font-bold lg:text-6xl font-cormorant-garamond"><span className="italic">BIZ</span> <span>  </span>  <span>  HAQIMIZDA</span></h1>
           <div className='w-full flex flex-col mt-10 items-center'>
             <h3 className='w-full lg:w-[1024px] text-center text-xl lg:text-5xl font-cormorant-garamond'>SIROJIDDIN ATTOR — bu original va sifatli parfumlar uyi. Biz dunyo brendlarini bir joyga jamlab, sizga o‘ziga xos va unutilmas iforlarni taqdim etamiz. Har bir mijozimizga e’tiborli yondashuv va ishonchli xizmat kafolatlanadi.</h3>
