ae571ded42d3513519914d4722e72c139fb00770
mayklweb <muhammadevvv@gmail.com>
2025-09-01 23:15:19 +0500

    m

--- src/app/home/about/index.tsx
+++ src/app/home/about/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 function About() {
@@ -9,7 +10,7 @@ function About() {
           <div className='w-full flex flex-col mt-10 items-center'>
             <h3 className='w-full lg:w-[1024px] text-center text-xl lg:text-5xl font-cormorant-garamond'>SIROJIDDIN ATTOR — bu original va sifatli parfumlar uyi. Biz dunyo brendlarini bir joyga jamlab, sizga o‘ziga xos va unutilmas iforlarni taqdim etamiz. Har bir mijozimizga e’tiborli yondashuv va ishonchli xizmat kafolatlanadi.</h3>
             <div className='w-full mt-10 rounded-full overflow-hidden'>
-              <img className='w-full h-full object-contain' src="/IMG_7295.webp" alt="" />
+              <Image width={1390} height={713} className='w-full h-full object-contain' src="/IMG_7295.webp" alt="" />
             </div>
           </div>
         </div>
