b73807800662e22a74768f219e7b858cdd724e99
mayklweb <muhammadevvv@gmail.com>
2025-09-01 23:13:33 +0500

    m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,27 +32,11 @@ const cormorantGaramond = Cormorant_Garamond({
 export const metadata: Metadata = {
   title: "Sirojiddin Attor - Parfyumeriya do'koni | Sirojiddin Attor - Магазин парфюмерии | Sirojiddin Attor - Perfume Shop",
   description: "Sirojiddin Attor - yuqori sifatli parfyumeriya mahsulotlarini taklif etuvchi do'kon. Bizning keng assortimentimizdan tanlang va o'zingizga yoqqan hidni toping. Sirojiddin Attor bilan har kuni o'zingizni maxsus his eting!",
-  icons: {
-    icon: "/favicon.ico",
-  },
   openGraph: {
     title: "Sirojiddin Attor - Parfyumeriya do'koni | Sirojiddin Attor - Магазин парфюмерии | Sirojiddin Attor - Perfume Shop",
     description: "Sirojiddin Attor - yuqori sifatli parfyumeriya mahsulotlarini taklif etuvchi do'kon. Bizning keng assortimentimizdan tanlang va o'zingizga yoqqan hidni toping. Sirojiddin Attor bilan har kuni o'zingizni maxsus his eting!",
     url: "https://sirojiddin-attor.uz",
     siteName: "Sirojiddin Attor",
-    images: [
-      {
-        url: "https://sirojiddin-attor.uz/og-image.jpg",
-        width: 800,
-        height: 600,
-      },
-      {
-        url: "https://sirojiddin-attor.uz/og-image.jpg",
-        width: 1800,
-        height: 1600,
-        alt: "Og Image Alt",
-      },
-    ],
     locale: "uz_UZ",
     type: "website",
   },
@@ -60,7 +44,6 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "Sirojiddin Attor - Parfyumeriya do'koni | Sirojiddin Attor - Магазин парфюмерии | Sirojiddin Attor - Perfume Shop",
     description: "Sirojiddin Attor - yuqori sifatli parfyumeriya mahsulotlarini taklif etuvchi do'kon. Bizning keng assortimentimizdan tanlang va o'zingizga yoqqan hidni toping. Sirojiddin Attor bilan har kuni o'zingizni maxsus his eting!",
-    images: ["https://sirojiddin-attor.uz/og-image.jpg"],
   },
 };
 
